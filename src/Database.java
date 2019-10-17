893777efe300fe829f9fe425e59fb727aa091d28
Emmanuel Sedicol <sedicolemmanuel@gmail.com>
2019-10-17 23:04:34 +0100

    new boolean method to check if input is either numeric or not

--- src/Database.java
+++ src/Database.java
@@ -123,4 +123,15 @@ public class Database {
 		window.frame.setVisible(false);
 		window.frame.setVisible(true);
 	} 
+
+
+	public boolean isNumeric(String str) {
+
+		for (char c : str.toCharArray()) {
+			if (!Character.isDigit(c)) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
