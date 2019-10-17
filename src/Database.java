190d9f50c7ac993142b5b781bf751046d2742343
Emmanuel Sedicol <sedicolemmanuel@gmail.com>
2019-10-17 22:32:24 +0100

    implemented methods to perform SQL insert, update and get Users

--- src/Database.java
+++ src/Database.java
@@ -1,11 +1,15 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Properties;
 
-public class Database {
+import javax.swing.JOptionPane;
 
+public class Database {
+	ArrayList<User> employees = getUsersList();
 
 	public Database() {
 	}
@@ -36,5 +40,81 @@ public class Database {
 				st.close(); 
 			}
 		}
+	}
+
+
+	// ************* SQL INSERT ************** //
+	public void add(int id, int ssn, String fname, String lname, String email) {
+		try{
+			String query = "INSERT INTO `employee`(`id`, `ssn`, `fname`, `lname`, `email`) VALUES (" + "'" +id+"'" + "," + "'"+ssn+"'" + "," + "'"+fname+"'" + "," + "'"+lname+"'" + "," + "'"+email+"'" + ");";
+			execute(query);
+			JOptionPane.showMessageDialog(null, fname + " " + lname + " Succesfully Added");
+//			refresh();
+		} catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+
+	// ************* SQL UPDATE ************** //
+	public void update(int ssn, String fname, String lname, String email) {
+		try{
+			String query = "UPDATE `employee` SET `ssn`= '" + ssn + "', `fname`= '" + fname + "', `lname`= '" + lname + "', `email`= '" + email + "' WHERE fname = '" + fname + "';";
+			execute(query);
+			JOptionPane.showMessageDialog(null, fname + " " + lname + " Succesfully Updated");
+//			refresh();
+		} catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+
+
+	// *********** List user in the DB by its ID *********** //
+	public User search (int id) {
+		User employee = null;
+
+		try {
+			Connection conn = getConnection();
+
+			// MySQL Query //
+			String query = "SELECT * FROM employee WHERE id = '" + id + "';";
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery(query);
+
+			while(rs.next()) {
+				employee = new User(rs.getInt("id"), rs.getInt("ssn"), rs.getString("fname"), rs.getString("lname"), rs.getString("email"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "SQL Query Failed :)");
+		}	
+		return employee;
+	}
+
+	// ************ Return a list of employees from database ************* //
+	public ArrayList<User> getUsersList()
+	{
+		ArrayList<User> usersList = new ArrayList<User>();
+
+		try {
+			Connection connection = getConnection();
+			Statement st;
+			ResultSet rs;
+			User user;
+
+			String query = "SELECT * FROM  `Employee` ";
+
+			st = connection.createStatement();
+			rs = st.executeQuery(query);
+
+			while(rs.next())
+			{
+				user = new User(rs.getInt("id"),rs.getInt("ssn"),  rs.getString("fname"),rs.getString("lname"),rs.getString("email"));
+				usersList.add(user);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return usersList;
 	}
 }
