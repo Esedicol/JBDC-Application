2c8d4476f53c1d1914ee1c0401843bcb2cb05cc1
Emmanuel Sedicol <sedicolemmanuel@gmail.com>
2019-10-17 21:27:50 +0100

    new class MainFrame. The class will be where I design the GUI using JAVA Swing

---
+++ src/MainFrame.java
@@ -0,0 +1,51 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+@SuppressWarnings("serial")
+public class MainFrame extends JPanel{
+
+	public JFrame frame;
+	private JTextField txtSsn;
+	private JTextField txtFirstName;
+	private JTextField txtLastName;
+	private JTextField txtEmail;
+	private JTextField txtId;
+
+
+	// Launch the application //
+	public static void main(String[] args) {
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame window = new MainFrame();
+
+					window = new MainFrame();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+
+	/**
+	 * Create the application.
+	 */
+	public MainFrame() {
+		initialize();
+	}
+
+	// Initialize the contents of the frame //
+	public void initialize() {
+
+		frame =  new JFrame("MySQL CRUD");
+		frame.setBounds(100, 100, 500, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+	}
+}
