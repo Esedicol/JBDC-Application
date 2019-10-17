fd3d674758782675c0a1cc86bb015079d5c05ac3
Emmanuel Sedicol <sedicolemmanuel@gmail.com>
2019-10-17 21:21:30 +0100

    Added getters and setter for all variables in the User object class

--- src/User.java
+++ src/User.java
@@ -1,4 +1,54 @@
 
 public class User {
+	
+	private int ID, SSN;
+	private String fName, lName, email;
+	
+	public User(int id, int ssn, String firstName, String lastName, String email) {
+		this.ID = id;
+		this.SSN = ssn;
+		this.fName = firstName;
+		this.lName = lastName;
+		this.email = email;
+	}
 
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+	
+	public int getSSN() {
+		return SSN;
+	}
+
+	public void setSSN(int sSN) {
+		SSN = sSN;
+	}
+
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
