3ec2bf591f081d617abfdbfc8f966014907abf8f
Emmanuel Sedicol <sedicolemmanuel@gmail.com>
2019-10-20 20:37:18 +0100

    Added CRUD Buttonsgi

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -52,7 +52,7 @@ public class MainFrame extends JPanel{
 		frame.getContentPane().setLayout(null);
 
 
-				// ***************** Labels ***************** //
+		// ***************** Labels ***************** //
 		JLabel lblSsn = new JLabel("SSN");
 		lblSsn.setBounds(30, 40, 80, 40);
 		frame.getContentPane().add(lblSsn);
@@ -68,5 +68,74 @@ public class MainFrame extends JPanel{
 		JLabel lblEmail = new JLabel("Email");
 		lblEmail.setBounds(30, 160, 80, 40);
 		frame.getContentPane().add(lblEmail);
+
+
+		// ***************** Buttons ***************** //
+
+		JButton btnAdd = new JButton("ADD");
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		}
+		btnAdd.setBounds(30, 210, 100, 50);
+		frame.getContentPane().add(btnAdd);
+
+
+		JButton btnUpdate = new JButton("UPDATE");
+		btnUpdate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		}
+		btnUpdate.setBounds(140, 210, 100, 50);
+		frame.getContentPane().add(btnUpdate);
+
+		JButton btnDelete = new JButton("Delete");
+		btnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		}
+		btnDelete.setBounds(250, 210, 100, 50);
+		frame.getContentPane().add(btnDelete);
+
+		JButton btnClear = new JButton("CLEAR");
+		btnClear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		}
+		btnClear.setBounds(360, 210, 100, 50);
+		frame.getContentPane().add(btnClear);
+
+		JButton btnSearch = new JButton("SEARCH");
+		btnSearch.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String inputID = txtId.getText().strip();
+
+				if(!inputID.equals("")) {
+					if(db.checkID(Integer.parseInt(inputID)) == false || db.isNumeric(inputID) == false) {
+						JOptionPane.showMessageDialog(null, "ERROR ID not found" );
+						db.refresh();
+					}
+
+					else if (Integer.parseInt(inputID) > size + 1) {
+						System.out.println("fail");
+						JOptionPane.showMessageDialog(null, "ID Out of Bounds, please enter numbers below: " + size );
+						db.refresh();
+					}
+
+					else {
+						User user = db.search(Integer.parseInt(inputID));
+						txtSsn.setText(Integer.toString(user.getSSN()));
+						txtFirstName.setText(user.getfName());
+						txtLastName.setText(user.getlName());
+						txtEmail.setText(user.getEmail());
+					}
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "Please enter an ID" );
+				}
+			}
+		});
+		btnSearch.setBounds(255, 166, 160, 30);
+		frame.getContentPane().add(btnSearch);
 	}
 }
